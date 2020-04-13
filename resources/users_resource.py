76c6dd71de6414836b48477f42881b8d7e22e13e
Soltan <soltan.shamgunov@yandex.ru>
2020-04-13 21:23:59 +0300

    beginning_2

---
+++ resources/users_resource.py
@@ -0,0 +1,61 @@
+from flask import jsonify
+from flask_restful import abort, Resource, reqparse
+
+from data import db_session
+from data.users import User
+
+parser = reqparse.RequestParser()
+parser.add_argument('email', required=True)
+parser.add_argument('name', required=True)
+parser.add_argument('surname', required=True)
+parser.add_argument('age', required=True, type=int)
+parser.add_argument('about', required=True)
+
+
+def abort_if_news_not_found(user_id):
+    session = db_session.create_session()
+    news = session.query(User).get(user_id)
+    if not news:
+        abort(404, message=f"User {user_id} not found")
+
+
+class UsersResource(Resource):
+    def get(self, user_id):
+        abort_if_news_not_found(user_id)
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        return jsonify({'users': user.to_dict(
+            only=('email', 'name', 'surname', 'age', 'about'))})
+
+    def delete(self, user_id):
+        abort_if_news_not_found(user_id)
+        session = db_session.create_session()
+        news = session.query(User).get(user_id)
+        session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UsersListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(User).all()
+        return jsonify({'users': [item.to_dict(
+            only=('email', 'name', 'surname', 'age', 'about')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        user = User(
+            surname=args['surname'],
+            name=args['name'],
+            age=args['age'],
+            about=args['about'],
+            email=args['email']
+        )
+        user.set_password(args['password'])
+        session.add(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import os
+
+import requests
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_restful import Api
+from werkzeug.utils import redirect
 
 from data import db_session
 from data.users import User
+from forms.reg_user import RegisterForm
+from resources import users_resource
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,6 +17,10 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+PORT = int(os.environ.get("PORT", 5000))
+
+api.add_resource(users_resource.UsersResource, '/api/users/<int:user_id>')
+api.add_resource(users_resource.UsersListResource, '/api/users')
 
 
 @login_manager.user_loader
@@ -19,9 +29,28 @@ def load_user(user_id):
     return session.query(User).get(user_id)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        res = requests.post(f'http://localhost:{PORT}/api/users', json={
+            'email': form.email.data,
+            'name': form.name.data,
+            'surname': form.surname.data,
+            'age': form.age.data,
+            'about': '',
+            'password': form.password.data
+        }).json()
+        if 'message' in res:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message=res['message'])
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 def main():
     db_session.global_init("db/data.sqlite")
-    app.run()
+    app.run(host='127.0.0.1', port=PORT)
 
 
 if __name__ == '__main__':
