5bcf6cd670ebe9f06050d8e96944ee7693bf2e02
Soltan <soltan.shamgunov@yandex.ru>
2020-04-19 20:19:32 +0300

    requirements

--- forms/day_tasks.py
+++ forms/day_tasks.py
@@ -7,6 +7,6 @@ from wtforms.validators import DataRequired
 class TaskForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     about = TextAreaField("Описание")
-    is_finished = BooleanField("Законченость")
+    is_finished = BooleanField("Законченность")
     submit = SubmitField('Добавить')
 
--- forms/gl_aims.py
+++ forms/gl_aims.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 class AimForm(FlaskForm):
     name = StringField('Цель', validators=[DataRequired()])
     about = TextAreaField("Описание")
-    is_finished = BooleanField("Законченость")
+    is_finished = BooleanField("Законченность")
     finish_date = StringField('Дата окончания. Обязательно в формате ГГГГ.ММ.ДД', validators=[DataRequired()])
     submit = SubmitField('Добавить')
 
